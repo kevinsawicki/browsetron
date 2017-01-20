0fc23f4bda4f61ba748d9f5095af540a3fb5b216
Kevin Sawicki <kevinsawicki@gmail.com>
2017-01-20 14:05:07 -0800

    Add context menu to open externally

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu, shell} = require('electron')
 const path = require('path')
 const url = require('url')
 
@@ -11,4 +11,22 @@ app.once('ready', () => {
    protocol: 'file:',
    slashes: true
  }))
+})
+
+app.on('web-contents-created', (event, contents) => {
+  if (contents.getType() !== 'webview') return
+  contents.on('context-menu', (event, {linkURL, x, y}) => {
+    if (!linkURL) return
+
+    const menu = Menu.buildFromTemplate([
+      {
+        label: 'Open Externally',
+        click: () => {
+          shell.openExternal(linkURL)
+        }
+      }
+    ])
+    const window = BrowserWindow.fromWebContents(contents.hostWebContents)
+    menu.popup(window, x, y)
+  })
 })
